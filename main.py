6500b58d6a64c25223dbcd69f1883b19e082c8fd
Annanikol <annanikol.kam@gmail.com>
2025-06-25 19:51:54 +0000

    Добавила отладочные сообщения в main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import threading
 import asyncio
+import time
+
 from server import app as flask_app
 from bot import main as telegram_main
 
 # Запускаем Flask-сервер
 def run_flask():
+    print("🚀 Flask запускается...")
     flask_app.run(host="0.0.0.0", port=8080)
 
 # Запускаем Telegram-бота
 def run_telegram():
-    # запускаем асинхронную функцию корректно
+    print("🤖 Telegram бот запускается...")
     asyncio.run(telegram_main())
 
 if __name__ == "__main__":
+    print("💡 Запуск приложения...")
     threading.Thread(target=run_flask).start()
-    import time
-    time.sleep(2)  # 🕒 даём Flask время запуститься
+    time.sleep(2)
     run_telegram()
