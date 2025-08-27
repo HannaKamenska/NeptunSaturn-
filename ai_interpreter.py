c0190ee9bccb984ce041cb501af8727fddd4048e
Annanikol <annanikol.kam@gmail.com>
2025-08-27 22:26:19 +0200

    версия гпт5 не работает, вернула гпт4

--- ai_interpreter.py
+++ ai_interpreter.py
@@ -82,7 +82,7 @@ def generate_transit_message(neptune, saturn, mars, jupiter, aspects):
 
     try:
         chat_completion = client.chat.completions.create(
-            model="gpt-5",
+            model="gpt-4",
             messages=[
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": user_context}
